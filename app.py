9888df52c43bba37beeddad0da6aa7757c73a400
chizuchizu <ochimasumi@gmail.com>
2019-08-30 21:06:03 +0900

    title変更

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, make_response, render_template, request, redirect, url_
 import pandas as pd
 import random
 
-df = pd.read_csv("questions.csv", header=None, index_col=None)
+df = pd.read_csv("qs.csv", header=None, index_col=None)
 print(df)
 count = 1
 
@@ -15,7 +15,7 @@ def title():
     if request.method == "GET":
         return render_template("title.html")
     else:
-        return redirect(url_for)
+        return redirect(url_for("quiz"))
 
 
 @app.route("/question", methods=["GET", "POST"])
@@ -24,37 +24,39 @@ def quiz():
     ans1 = random.randint(0, 1)
     ans2 = 1 - ans1
     if request.method == "GET":
-        # print(count)
-        print("GET")
         return render_template("index.html", number=count, ans1=df.iloc[count - 1, ans1 + 1],
                                ans2=df.iloc[count - 1, ans2 + 1], question=df.iloc[count - 1, 0])
     else:
-        print(request.form["1"])
-        print("POST")
         memo = int(request.form["1"])
         count += 1
         print(ans1, memo)
+        ex = df.iloc[count - 1, 3]
+        cr = df.iloc[count - 1, 2]
         if ans1 == 1 and memo == 0 or ans1 == 0 and memo == 1:
             print("correct answer")
-            return redirect(url_for("correct"))
+            return redirect(url_for("correct", ans1=cr, ans2=df.iloc[count - 2, memo + 1],
+                                    explain=ex))
         else:
-            return redirect(url_for("wrong"))
+            print("wrong answer")
+            return redirect(url_for("wrong", ans1=cr, ans2=df.iloc[count - 2, memo + 1],
+                                    explain=ex))
 
 
-@app.route("/correct", methods=["GET", "POST"])
-def correct():
-    print("aa")
+@app.route("/correct/<string:ans1><string:ans2><string:explain>", methods=["GET", "POST"])
+def correct(ans1, ans2, explain):
+    print(ans1)
+    print(ans2)
+    print(explain)
     if request.method == "GET":
-        return render_template("correct.html")
+        return render_template("correct.html", ans1=ans1, ans2=ans2, explain=explain)
     else:
         return redirect(url_for("question"))
 
 
-@app.route("/wrong")
-def wrong():
-    print("bb")
+@app.route("/wrong/<string:ans1><string:ans2><string:explain>", methods=["GET", "POST"])
+def wrong(ans1, ans2, explain):
     if request.method == "GET":
-        return render_template("wrong.html")
+        return render_template("wrong.html", ans1=ans1, ans2=ans2, explain=explain)
     else:
         return redirect(url_for("question"))
 
